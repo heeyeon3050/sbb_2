a3a78901ffa645c575e3507977a939420862c4d4
heeyeon3050 <kho200a@naver.com>
2023-03-28 11:44:58 +0900

    컨트롤러

---
+++ src/main/java/com/mysite/sbb_2/MainController.java
@@ -0,0 +1,18 @@
+package com.mysite.sbb_2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    //서버에 요청이 발생하면 스프링부트는 요청 페이지와 매핑되는 메서드를 컨트롤러를 대상으로 찾는다.
+    //스프링부트는 http://localhost:8080/sbb 요청이 발생하면 /sbb URL과 매핑되는 index 메서드를 MainController에서 찾아 실행한다.
+    @GetMapping("/sbb") //URL 매핑
+    @ResponseBody //URL 요청에 대한 응답으로 문자열을 리턴하라는 의미 //@ResponseBody를 생략하면, "index"라는 파일을 찾게 됨
+    public String index() {
+        System.out.println("index");
+        return "index";
+    }
+}
--- src/main/java/com/mysite/sbb_2/Sbb2Application.java
+++ src/main/java/com/mysite/sbb_2/Sbb2Application.java
@@ -3,6 +3,7 @@ package com.mysite.sbb_2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//시작을 담당하는 파일
 @SpringBootApplication
 public class Sbb2Application {
 
