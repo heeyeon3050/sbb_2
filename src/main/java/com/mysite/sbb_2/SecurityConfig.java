0abef999f7eca9176d0ae807588e624f9d6409d1
heeyeon3050 <kho200a@naver.com>
2023-03-30 11:30:48 +0900

    스프링 시큐리티

---
+++ src/main/java/com/mysite/sbb_2/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.mysite.sbb_2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration //스프링의 환경설정 파일임을 의미
+@EnableWebSecurity //모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만듦
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers(
+                new AntPathRequestMatcher("/**")).permitAll()
+        ;
+        return http.build();
+    }
+}
